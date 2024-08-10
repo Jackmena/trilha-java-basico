91b37a4c4314f287e418b8edc5e08e153abe1dcb
Jackson Mena <mena.jack72@gmail.com>
2024-08-10 19:47:52 -0300

    tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // tipos primitivos
+
+        byte idade = 32;
+        short ano = 2024;
+        int cep = 12345678; // se começar com zero, talvez tenha que mudar o tipo
+        long cpf = 987654321L; // se começar com zero, talvez tenha que mudar o tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        String meuNome = "Jackson";
+
+        boolean aprovado = true;
+        
+
+        // variavel que não pode ser mudada
+
+        final int DATA_NASCIMENTO = 1992;
     }
 }
